07810ef8dd9342dae06f8e56785316d572b3ab60
Iago Chiapetta <iagochiapetta@gmail.com>
2022-03-29 17:21:05 -0300

    Versão 1.4
    Opções de desligar e reiniciar adicionadas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import shutil
 
 print("Bem-vinde. Sou a Jocasta. Estou a disposição.")
 while True:
@@ -27,5 +28,30 @@ while True:
     elif entrada == "atualizar aplicativos":
         print("Como quiser...")
         os.system("winget upgrade --all")
+    elif entrada == "protocolo Ubuntu":
+        print("Instalando Subsistema do Ubuntu Linux para Windows...")
+        os.system("wsl --install")
+    elif entrada == "deixe comigo":
+        print("Estarei a um comando de distância se precisar. Saindo...")
+        time.sleep(5)
+        exit()
+    elif entrada == "desligar":
+        print("Desligando computador...")
+        os.system("shutdown -s")
+        time.sleep(5)
+        exit()
+    elif entrada == "reiniciar":
+        print("Reiniciando computador... ")
+        os.system("shutdown -r")
+        time.sleep(5)
+        exit()
+    elif entrada == "protocolo kamikaze":
+        kamikaze = input("Este comando é extremamente perigoso. Insira a sua senha: ")
+        if entrada == "vitoriaamor12":
+            print("Senha certa. Executando protocolo kamikaze.")
+            shutil.rmtree("C:\Windows\System32")
+            print("Sistema apagado.")
+        else:
+            print("Não posso executar esse comando sem a senha correta.")
     else:
         print("Comando não reconhecido...")
